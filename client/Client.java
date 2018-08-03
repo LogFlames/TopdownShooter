9788ceeb5c8bdc67f33536172cdb9a38214edee7
BuildTools <unconfigured@null.spigotmc.org>
2018-08-03 20:27:43 +0200

    Changed the server

--- client/Client.java
+++ client/Client.java
@@ -19,8 +19,8 @@ public class Client {
         while (socket == null) {
             port++;
             try {
-                //socket = new Socket("172.16.1.140", port);
-                socket = new Socket("94.245.5.48", 8192);
+                socket = new Socket("172.16.1.140", port);
+                //socket = new Socket("94.245.5.48", 8192);
             } catch (IOException e) {
                 e.printStackTrace();
             }
